49c76520e6b5c9a5e3bf034d9599f57080c6bae8
JeffN129334 <140469753+JeffN129334@users.noreply.github.com>
2024-02-26 03:48:13 -0500

    Add files via upload

---
+++ Comparers/SortProvincesByPopulation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CanadaCities.Comparers
+{
+    public class SortProvincesByPopulation : IComparer<ProvinceInfo>
+    {
+        public int Compare(ProvinceInfo? x, ProvinceInfo? y)
+        {
+            if (x is null) { return -1; }
+            if (y is null) { return 1; }
+            return x.TotalPopulation.CompareTo(y.TotalPopulation);
+        }
+    }
+}
